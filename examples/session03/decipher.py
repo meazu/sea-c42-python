01cd402700b83559a951d2d6990950e8870b2937
meazu <meazu0@gmail.com>
2015-06-22 23:09:17 -0700

    Added cipher.py and decipher.py

--- examples/session03/decipher.py
+++ examples/session03/decipher.py
@@ -6,12 +6,12 @@ alphabet_list = list(alphabet)
 
 # Copy and paste the cipher list from your partner below,
 # instead of the empty list.
-cipher_list = ['v', 'w', 'u', 'q', 'k', 'm', 'g', 'z', 'l', 'n', 'e', ' ', 'd', 'b', 'x', 'y', 'i', 't', 'f', 'h', 'r', 'c', 'j', 'a', 'p', 's', 'o']
+cipher_list = ['p', 'a', 'y', 'o', 'u', 'l', 'n', 'b', 'i', 't', 'z', 'd', 'g', ' ', 'c', 'w', 'j', 'e', 'r', 'q', 'k', 'v', 'f', 'x', 's', 'm', 'h']
 
 
 # copy and paste the cipher_text from your partner in slack
 # and assign it to the variable new_cipher_text
-new_cipher_text = "jzvhozvfoqvhkfovbqofrbqvpfowrhouvbbxhowkokvhkb"
+new_cipher_text = "fbshirhrixhplepiohclhruvu"
 
 decrypt_text = ""
 
