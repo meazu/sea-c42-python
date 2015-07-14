2e47bb47112f479248b2b83fbd6e44eec6317bf7
meazu <meazu0@gmail.com>
2015-07-13 18:57:56 -0700

    Added comments to the break_me.py file

--- hw/hw04/break_me.py
+++ hw/hw04/break_me.py
@@ -4,14 +4,14 @@ def exhibit_name_error():
     sum = a + b # 'b' is not defined and trying sum it with 'a'
     return sum
 
-#Function when called throws a type exception        
+#Function when called throws a type exception.        
 def exhibit_type_error():
     a = 10
     b = 'Apple'
     div = a // b # value of 'a' is int and 'b' is string type and 
                      # performing divions operation.
     return div
-
+#Function when called throws an attribute exception.
 def exhibit_attribute_error():
     a = [10, 20, 30]
     b = 'string'
