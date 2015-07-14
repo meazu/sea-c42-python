fcab822dd3aa1b8dbf93e30837b6a2ccfffac2ef
meazu <meazu0@gmail.com>
2015-07-13 18:56:14 -0700

    Added break_me.py and test_break_me.py files of homework 4

---
+++ hw/hw04/break_me.py
@@ -0,0 +1,18 @@
+#Function when called throws a name exception.
+def exhibit_name_error():
+    a = 10   
+    sum = a + b # 'b' is not defined and trying sum it with 'a'
+    return sum
+
+#Function when called throws a type exception        
+def exhibit_type_error():
+    a = 10
+    b = 'Apple'
+    div = a // b # value of 'a' is int and 'b' is string type and 
+                     # performing divions operation.
+    return div
+
+def exhibit_attribute_error():
+    a = [10, 20, 30]
+    b = 'string'
+    return (a.b)
